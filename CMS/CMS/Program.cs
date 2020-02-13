cceac750b8cc5738201867ff86b7a0a6fcd85a7c
AdamKeeley <a.keeley@leeds.ac.uk>
2020-02-13 00:05:20 +0000

    Added some DateTime fields; changed insertProject method to handle them with null values. Normalised [tblProjects].[Stage] field; added a second table to DataSet and used DataRelation to link the two. Nested foreach to return [pStageDescription] (string) from [pStage] (int).

--- CMS/CMS/Program.cs
+++ CMS/CMS/Program.cs
@@ -12,26 +12,30 @@ namespace CMS
         {
             //instantiate new Project type object that contains details of all projects
             var Projects = new Project();
+
+            //string pNumber = "P0007";
+            //string pName = "Project with int stage";
+            //int pStage = 2;
+            //string pPI = "Mr Inty";
+            //DateTime? pStartDate = Convert.ToDateTime("24/02/2020");
+            //DateTime? pEndDate = null;
+
             //call member method to display projects
             Console.WriteLine(Projects.getProjectsToString());
 
             Console.WriteLine("");
 
             ////add a new project
-            //Projects.insertProject("P0005", "An even newer project", "Setup", "Joe Pineapples");
+            //Projects.insertProject(pNumber, pName, pStage, pPI, pStartDate, pEndDate);
+
+            ////update existing project - first perform logical delete then insert new record
+            //Projects.deleteProject(pNumber);
+            //Projects.insertProject(pNumber, pName, pStage, pPI, pStartDate, pEndDate);
+
             ////refresh the DataSet
             //Projects.ds_Project = Projects.getProjectsDataSet();
             ////print to console
-            //Console.WriteLine(Projects.getProjects());
-
-            ////update existing project - first perform logical delete then insert new record
-            //Projects.deleteProject("P0004");
-            //Projects.insertProject("P0004", "A newer project", "Closed", "Sticky Bob");
-            
-            //refresh the DataSet
-            Projects.ds_Project = Projects.getProjectsDataSet();
-            //print to console
-            Console.WriteLine(Projects.getProjectsToString());
+            //Console.WriteLine(Projects.getProjectsToString());
         }
     }                                                        
 }
--- CMS/CMS/Project.cs
+++ CMS/CMS/Project.cs
@@ -27,16 +27,28 @@ namespace CMS
             SQL_Stuff conString = new SQL_Stuff();
             using (SqlConnection connection = new SqlConnection(conString.getString()))
             {
-                //use a DataAdapter (da_Project) to add a table (tblProjects) to a DataSet (ds_Project) 
+                //use a DataAdapter (da_Project) to add a DataTable (tblProjects) to a DataSet (ds_Project) 
                 //from a SQL Server query (qryGetProject)
-                //and put that DataSet (ds_prj) into the DataSet member object (ds_Project)
                 SqlCommand qryGetProject = new SqlCommand();
                 SqlDataAdapter da_Project = new SqlDataAdapter();
-                DataSet ds_prj = new DataSet();
-                qryGetProject.CommandText = $"select * from dbo.tblProject where ValidUntil is null order by [pNumber]";
+                DataSet ds_prj = new DataSet("Projects");
+                qryGetProject.CommandText = $"select * from [dbo].[tblProject] where [ValidTo] is null order by [pNumber]";
                 qryGetProject.Connection = connection;
                 da_Project.SelectCommand = qryGetProject;
                 da_Project.Fill(ds_prj, "tblProjects");
+
+                //add tlkStage DataTable to DataSet (ds_prj)
+                SqlCommand qryGetStage = new SqlCommand();
+                qryGetStage.CommandText = $"select * from [dbo].[tlkStage] where [ValidTo] is null";
+                qryGetStage.Connection = connection;
+                da_Project.SelectCommand = qryGetStage;
+                da_Project.Fill(ds_prj, "tlkStage");
+                //create a DataRelation (rltnProjectStage) to join tlkStage to tblProjects
+                ds_prj.Relations.Add("Project_Stage"
+                    , ds_prj.Tables["tlkStage"].Columns["StageID"]          //parent
+                    , ds_prj.Tables["tblProjects"].Columns["pStage"]);      //child
+
+                //return DataSet (ds_prj) as the output of this method
                 return ds_prj;
             }
         }
@@ -46,22 +58,29 @@ namespace CMS
         {
             string pNumber;
             string pName;
-            string pStage;
+            string pStage = string.Empty;
             string pPI;
+            string pStartDate;
+            string pEndDate;
 
             //create a new string (pListProjects) using StringBuilder
             var pListProjects = new StringBuilder();
             //add tab delimited (\t) headers as first row
-            pListProjects.AppendLine("Project Number\tProject Name\tStage\tPI");
+            pListProjects.AppendLine("Project Number\tProject Name\tStage\tPI\tStart Date\tEnd Date");
             //loop through each row in DataTable (dt_Project), assign each column value to a class property member 
             //and add a new row to the string (pListProjects)
-            foreach (DataRow row in ds_Project.Tables["tblProjects"].Rows)
+            foreach (DataRow pRow in ds_Project.Tables["tblProjects"].Rows)
             {
-                pNumber = row["pNumber"].ToString();
-                pName = row["pName"].ToString();
-                pStage = row["pStage"].ToString();
-                pPI = row["pPI"].ToString();
-                pListProjects.AppendLine($"{pNumber}\t{pName}\t{pStage}\t{pPI}");
+                pNumber = pRow["pNumber"].ToString();
+                pName = pRow["pName"].ToString();
+                foreach (DataRow sRow in pRow.GetParentRows("Project_Stage"))
+                {
+                    pStage = sRow["pStageDescription"].ToString();
+                }
+                pPI = pRow["pPI"].ToString();
+                pStartDate = pRow["pStartDate"].ToString();
+                pEndDate = pRow["pEndDate"].ToString();
+                pListProjects.AppendLine($"{pNumber}\t{pName}\t{pStage}\t{pPI}\t{pStartDate}\t{pEndDate}");
             }
             //output the complete string (pListProjects)
             return pListProjects.ToString();
@@ -73,7 +92,7 @@ namespace CMS
             //query the DataTable for the row identifier (pID) of current record of project to update
             int pID;
             DataRow[] dr_ProjectToUpdate;
-            dr_ProjectToUpdate = ds_Project.Tables["tblProjects"].Select($"pNumber = '{pNumber}' and ValidUntil is null");
+            dr_ProjectToUpdate = ds_Project.Tables["tblProjects"].Select($"pNumber = '{pNumber}' and ValidTo is null");
             pID = (int)dr_ProjectToUpdate[0]["pID"];
 
             //update ValidUntil field of current record of project (perform 'logical' delete)
@@ -82,7 +101,7 @@ namespace CMS
             {
                 SqlCommand qryDeleteProject = new SqlCommand();
                 qryDeleteProject.Connection = connection;
-                qryDeleteProject.CommandText = "update [dbo].[tblProject] set ValidUntil = getdate() where pID = @pID";
+                qryDeleteProject.CommandText = "update [dbo].[tblProject] set [ValidTo] = getdate() where [pID] = @pID";
                 qryDeleteProject.Parameters.Add("@pID", SqlDbType.Int).Value = pID;
                 connection.Open();
                 qryDeleteProject.ExecuteNonQuery();
@@ -91,25 +110,37 @@ namespace CMS
         }
 
         //method to insert new project
-        public void insertProject (string pNumber, string pName, string pStage, string pPI)
+        public void insertProject (string pNumber, string pName, int pStage, string pPI, DateTime? pStartDate, DateTime? pEndDate)
         {
             SQL_Stuff conString = new SQL_Stuff();
             using (SqlConnection connection = new SqlConnection(conString.getString()))
             {
+                //generate the parameterised SQL query to insert new record
                 SqlCommand qryInsertProject = new SqlCommand();
                 qryInsertProject.Connection = connection;
-                qryInsertProject.CommandText = "insert into [dbo].[tblProject] ([pNumber],[pName],[pStage],[pPI]) values "
-                    + "(@pNumber, @pName, @pStage, @pPI)";
+                qryInsertProject.CommandText = "insert into [dbo].[tblProject] ([pNumber],[pName],[pStage],[pPI],[pStartDate],[pEndDate]) values "
+                    + "(@pNumber, @pName, @pStage, @pPI, @pStartDate, @pEndDate)";
+
+                //assign the parameter values
                 qryInsertProject.Parameters.Add("@pNumber", SqlDbType.VarChar, 5).Value = pNumber;
                 qryInsertProject.Parameters.Add("@pName", SqlDbType.VarChar, 100).Value = pName;
                 qryInsertProject.Parameters.Add("@pStage", SqlDbType.VarChar, 20).Value = pStage;
                 qryInsertProject.Parameters.Add("@pPI", SqlDbType.VarChar, 60).Value = pPI;
+                //dates are fuckey
+                //while i'm not fussed about empty strings i want to avoid 1900-01-01 and use NULL for missing values
+                SqlParameter param_pStartDate = new SqlParameter("@pStartDate", pStartDate == null ? (object)DBNull.Value : pStartDate);
+                param_pStartDate.IsNullable = true;
+                qryInsertProject.Parameters.Add(param_pStartDate);
+                SqlParameter param_pEndDate = new SqlParameter("@pEndDate", pEndDate == null ? (object)DBNull.Value : pEndDate);
+                param_pEndDate.IsNullable = true;
+                qryInsertProject.Parameters.Add(param_pEndDate);
+
+                //open connection to database, run query and close connection
                 connection.Open();
                 qryInsertProject.ExecuteNonQuery();
                 connection.Close();
             }
         }
-
     }        
 }
 
