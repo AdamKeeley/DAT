9f62e22df07cb2561966f7cfc0a75ff5d3379be7
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-18 14:01:51 +0100

    Rewrite get assets history method to use base c# and DataSet objects The previous implementation used Dapper. We've removed this to avoid any third-party dependencies. The new method uses base C# code, opening the DB connection and passing each table to a DataTable, via a DataAdapter, which are added to a DataSet. The DataSet filling is handled by a helper method. Relations between the DataTables are added, although they aren't currently used downstream. (LINQ joins specify the relations instead when creating the assets history view.)

--- CMS/CMS/DataIO.cs
+++ CMS/CMS/DataIO.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 using DataControlsLib;
 using DataControlsLib.DataModels;
 
@@ -12,40 +13,38 @@ namespace CMS
 {
     public class DataIO
     {
-        public DataSet GetAssetsHistory(DateTime? dateFrom = null, DateTime? dateTo = null, string projNumber = null, string vreFilePath = null)
+        public DataSet GetAssetsHistoryDataSet()
         {
-            throw new NotImplementedException();
-        }
-        
-        /*public List<FullAssetsHistoryModel> GetAssetsHistory(
-            DateTime? dateFrom = null, DateTime? dateTo = null, string projNumber = null, string vreFilePath = null
-            )
-        {
+            DataSet ds_io = new DataSet("AssetsHistory");
+
             SQL_Stuff conString = new SQL_Stuff();
-            using (IDbConnection conn = new System.Data.SqlClient.SqlConnection(conString.getString()))
+            using (SqlConnection connection = new SqlConnection(conString.getString()))
             {
-                conn.Open();
+                GetDB.GetDataTable(connection, ds_io, "tblDataIORequests",
+                    "SELECT RequestID, Project, ChangeDate, ChangeType, RequestedBy, ChangedBy FROM dbo.tblDataIORequests ORDER BY ChangeDate DESC");
+                GetDB.GetDataTable(connection, ds_io, "tblAssetsRegister", 
+                    "SELECT AssetID, AssetName, AssetSha256sum, VreFilePath FROM dbo.tblAssetsRegister");
+                GetDB.GetDataTable(connection, ds_io, "tblAssetsChangeLog",
+                    "SELECT ChangeID, RequestID, AssetID, ChangeAccepted, RejectionNotes FROM dbo.tblAssetsChangeLog");
+                GetDB.GetDataTable(connection, ds_io, "tlkAssetChangeTypes", 
+                    "SELECT ChangeTypeID, ChangeTypeLabel FROM dbo.tlkAssetChangeTypes");
+                GetDB.GetDataTable(connection, ds_io, "tblProject",
+                    "SELECT * FROM dbo.tblProject WHERE ValidTo IS NULL");
 
-                var pars = new DynamicParameters();
-                pars.Add(name: "@DateFrom", value: dateFrom, dbType: DbType.DateTime);
-                pars.Add(name: "@DateTo", value: dateTo, dbType: DbType.DateTime);
-                pars.Add(name: "@ProjectNumber", value: projNumber, dbType: DbType.String, size: 5);
-                pars.Add(name: "@VreFilePath", value: vreFilePath, dbType: DbType.String, size: 200);
-
-                return conn.Query<FullAssetsHistoryModel, AssetsRegisterModel, DataIOModel, ProjectModel, tlkAssetChangeTypeModel, FullAssetsHistoryModel>(
-                    sql: "dbo.spDataIO_GetFullAssetsHistory @DateFrom, @DateTo, @ProjectNumber, @VreFilePath",
-                    map: (history, reg, io, proj, tlk) => {
-                        history.AssetsRegister = reg;
-                        history.DataIO = io;
-                        history.Project = proj;
-                        history.AssetChangeTypes = tlk;
-                        return history;
-                    },
-                    param: pars,
-                    splitOn: "AssetSha256sum,ChangeDate,ProjectNumber,ChangeTypeLabel")
-                    .ToList();
+                ds_io.Relations.Add("DataIORequests_AssetChangeTypes", 
+                    parentColumn: ds_io.Tables["tlkAssetChangeTypes"].Columns["ChangeTypeID"],
+                    childColumn: ds_io.Tables["tblDataIORequests"].Columns["ChangeType"]
+                );
+                ds_io.Relations.Add("AssetsChangeLog_DataIORequests",
+                    parentColumn: ds_io.Tables["tblDataIORequests"].Columns["RequestID"],
+                    childColumn: ds_io.Tables["tblAssetsChangeLog"].Columns["RequestID"]
+                );
+                ds_io.Relations.Add("AssetsChangeLog_AssetsRegister",
+                    parentColumn: ds_io.Tables["tblAssetsRegister"].Columns["AssetID"],
+                    childColumn: ds_io.Tables["tblAssetsChangeLog"].Columns["AssetID"]
+                );
             }
-
-        }*/
+            return ds_io;
+        }
     }
 }
