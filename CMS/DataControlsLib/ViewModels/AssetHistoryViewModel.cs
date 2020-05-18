5f6b4bd33202ff4b16cb86353485d35763f8df57
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-18 13:40:29 +0100

    Add data model for views of the data

---
+++ CMS/DataControlsLib/ViewModels/AssetHistoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControlsLib.ViewModels
+{
+    public class AssetHistoryViewModel
+    {
+        public string Project { get; set; }
+        public DateTime? ChangeDate { get; set; }
+        public string ChangeType { get; set; }
+        public string AssetName { get; set; }
+        public string FilePath { get; set; }
+        public string Checksum { get; set; }
+        public bool? ChangeAccepted { get; set; }
+        public string RequestedBy { get; set; }
+        public string ChangedBy { get; set; }
+    }
+}
