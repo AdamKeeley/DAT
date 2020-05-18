2c6308a6aa9d35bedfce68b8120678eeb8868334
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-18 15:48:46 +0100

    Create Homepage to direct towards Project or DataTracking forms

---
+++ CMS/CMS/frm_HomePage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMS
+{
+    public partial class frm_HomePage : Form
+    {
+        public frm_HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_GoToProjects_Click(object sender, EventArgs e)
+        {
+            frm_ProjectAll ProjectAllForm = new frm_ProjectAll();
+            ProjectAllForm.Show();
+        }
+
+        private void btn_GoToDataIO_Click(object sender, EventArgs e)
+        {
+            frm_DataIO DataIOForm = new frm_DataIO();
+            DataIOForm.Show();
+        }
+    }
+}
