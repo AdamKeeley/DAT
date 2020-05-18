390543394c5e5b0b5be1be2f1c28352e07f93ca3
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-18 15:50:59 +0100

    Load homepage on startup instead of Project form

--- CMS/CMS/Program.cs
+++ CMS/CMS/Program.cs
@@ -73,7 +73,7 @@ namespace CMS
             //initialise form
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_DataIO());
+            Application.Run(new frm_HomePage());
         }
     }                                                 
 }
