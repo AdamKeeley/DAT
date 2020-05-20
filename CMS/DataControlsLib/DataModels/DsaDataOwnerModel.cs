5f0a41b133bac630ec6d25428b2f66e84de39daf
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-20 15:12:48 +0100

    Add data models for DSA tables

---
+++ CMS/DataControlsLib/DataModels/DsaDataOwnerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControlsLib.DataModels
+{
+    public class DsaDataOwnerModel
+    {
+        public int doID { get; set; }
+        public int? RebrandOf { get; set; }
+        public string DateOwnerName { get; set; }
+
+    }
+}
---
+++ CMS/DataControlsLib/DataModels/DsasProjectsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControlsLib.DataModels
+{
+    public class DsasProjects
+    {
+        public int dpID { get; set; }
+        public int DsaID { get; set; }
+        public string Project { get; set; }
+        public DateTime? DateAdded { get; set; }
+    }
+}
