d2d00d902aa4a7360459558af03440e699f56ed0
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-20 17:07:47 +0100

    Add data model for amendmentOf View in frm_DsaAdd

---
+++ CMS/DataControlsLib/ViewModels/DsaBasicsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControlsLib.ViewModels
+{
+    public class DsaBasicsViewModel
+    {
+        public string DataOwner { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public string DsaName { get; set; }
+        public string FilePath { get; set; }
+        public string AmendmentOf { get; set; }
+        public bool DSPT { get; set; }
+        public bool ISO270001 { get; set; }
+    }
+}
