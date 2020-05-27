9b204020832d47feb4a6a84e405a3cf5d33de124
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-27 16:57:14 +0100

    Add view data model for viewing data owners list

---
+++ CMS/DataControlsLib/ViewModels/DataOwnersViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataControlsLib.ViewModels
+{
+    public class DataOwnersViewModel
+    {
+        public string DataOwner { get; set; }
+        public string RebrandingOf { get; set; }
+    }
+}
