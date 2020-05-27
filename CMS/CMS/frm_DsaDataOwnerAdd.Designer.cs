13b8859ad56f94868609747e89ae38bb84285e4e
seantuck12 <s.tuck@leeds.ac.uk>
2020-05-27 16:58:00 +0100

    Create form for adding new data owner

---
+++ CMS/CMS/frm_DsaDataOwnerAdd.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CMS
+{
+    partial class frm_DsaDataOwnerAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_NewDataOwnerName = new System.Windows.Forms.TextBox();
+            this.btn_NewDataOwner = new System.Windows.Forms.Button();
+            this.gb_DataOwners = new System.Windows.Forms.GroupBox();
+            this.dgv_DataOwners = new System.Windows.Forms.DataGridView();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.tb_Search = new System.Windows.Forms.TextBox();
+            this.gb_AddDataOwner = new System.Windows.Forms.GroupBox();
+            this.lbl_NewDataOwnerName = new System.Windows.Forms.Label();
+            this.lbl_RebrandingOfOldName = new System.Windows.Forms.Label();
+            this.cb_RebrandingOfOldName = new System.Windows.Forms.ComboBox();
+            this.gb_DataOwners.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_DataOwners)).BeginInit();
+            this.gb_AddDataOwner.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tb_NewDataOwnerName
+            // 
+            this.tb_NewDataOwnerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_NewDataOwnerName.Location = new System.Drawing.Point(231, 24);
+            this.tb_NewDataOwnerName.Name = "tb_NewDataOwnerName";
+            this.tb_NewDataOwnerName.Size = new System.Drawing.Size(367, 27);
+            this.tb_NewDataOwnerName.TabIndex = 0;
+            // 
+            // btn_NewDataOwner
+            // 
+            this.btn_NewDataOwner.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_NewDataOwner.Location = new System.Drawing.Point(452, 117);
+            this.btn_NewDataOwner.Name = "btn_NewDataOwner";
+            this.btn_NewDataOwner.Size = new System.Drawing.Size(199, 27);
+            this.btn_NewDataOwner.TabIndex = 1;
+            this.btn_NewDataOwner.Text = "Add Data Owner";
+            this.btn_NewDataOwner.UseVisualStyleBackColor = true;
+            // 
+            // gb_DataOwners
+            // 
+            this.gb_DataOwners.Controls.Add(this.btn_Search);
+            this.gb_DataOwners.Controls.Add(this.dgv_DataOwners);
+            this.gb_DataOwners.Controls.Add(this.tb_Search);
+            this.gb_DataOwners.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_DataOwners.Location = new System.Drawing.Point(39, 168);
+            this.gb_DataOwners.Name = "gb_DataOwners";
+            this.gb_DataOwners.Size = new System.Drawing.Size(724, 335);
+            this.gb_DataOwners.TabIndex = 2;
+            this.gb_DataOwners.TabStop = false;
+            this.gb_DataOwners.Text = "List of Existing Data Owners";
+            // 
+            // dgv_DataOwners
+            // 
+            this.dgv_DataOwners.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_DataOwners.Location = new System.Drawing.Point(6, 68);
+            this.dgv_DataOwners.Name = "dgv_DataOwners";
+            this.dgv_DataOwners.RowHeadersWidth = 51;
+            this.dgv_DataOwners.RowTemplate.Height = 24;
+            this.dgv_DataOwners.Size = new System.Drawing.Size(711, 261);
+            this.dgv_DataOwners.TabIndex = 0;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Search.Location = new System.Drawing.Point(452, 30);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(199, 27);
+            this.btn_Search.TabIndex = 4;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            // 
+            // tb_Search
+            // 
+            this.tb_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Search.Location = new System.Drawing.Point(59, 30);
+            this.tb_Search.Name = "tb_Search";
+            this.tb_Search.Size = new System.Drawing.Size(367, 27);
+            this.tb_Search.TabIndex = 3;
+            // 
+            // gb_AddDataOwner
+            // 
+            this.gb_AddDataOwner.Controls.Add(this.cb_RebrandingOfOldName);
+            this.gb_AddDataOwner.Controls.Add(this.lbl_RebrandingOfOldName);
+            this.gb_AddDataOwner.Controls.Add(this.lbl_NewDataOwnerName);
+            this.gb_AddDataOwner.Controls.Add(this.btn_NewDataOwner);
+            this.gb_AddDataOwner.Controls.Add(this.tb_NewDataOwnerName);
+            this.gb_AddDataOwner.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_AddDataOwner.Location = new System.Drawing.Point(39, 12);
+            this.gb_AddDataOwner.Name = "gb_AddDataOwner";
+            this.gb_AddDataOwner.Size = new System.Drawing.Size(724, 150);
+            this.gb_AddDataOwner.TabIndex = 3;
+            this.gb_AddDataOwner.TabStop = false;
+            this.gb_AddDataOwner.Text = "New Data Owner";
+            // 
+            // lbl_NewDataOwnerName
+            // 
+            this.lbl_NewDataOwnerName.AutoSize = true;
+            this.lbl_NewDataOwnerName.Location = new System.Drawing.Point(19, 24);
+            this.lbl_NewDataOwnerName.Name = "lbl_NewDataOwnerName";
+            this.lbl_NewDataOwnerName.Size = new System.Drawing.Size(148, 20);
+            this.lbl_NewDataOwnerName.TabIndex = 2;
+            this.lbl_NewDataOwnerName.Text = "Data Owner Name";
+            // 
+            // lbl_RebrandingOfOldName
+            // 
+            this.lbl_RebrandingOfOldName.AutoSize = true;
+            this.lbl_RebrandingOfOldName.Location = new System.Drawing.Point(19, 71);
+            this.lbl_RebrandingOfOldName.Name = "lbl_RebrandingOfOldName";
+            this.lbl_RebrandingOfOldName.Size = new System.Drawing.Size(157, 40);
+            this.lbl_RebrandingOfOldName.TabIndex = 3;
+            this.lbl_RebrandingOfOldName.Text = "Rebranding of Old\r\nData Owner Name?";
+            // 
+            // cb_RebrandingOfOldName
+            // 
+            this.cb_RebrandingOfOldName.FormattingEnabled = true;
+            this.cb_RebrandingOfOldName.Location = new System.Drawing.Point(231, 71);
+            this.cb_RebrandingOfOldName.Name = "cb_RebrandingOfOldName";
+            this.cb_RebrandingOfOldName.Size = new System.Drawing.Size(222, 28);
+            this.cb_RebrandingOfOldName.TabIndex = 4;
+            // 
+            // frm_DsaDataOwnerAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.gb_AddDataOwner);
+            this.Controls.Add(this.gb_DataOwners);
+            this.Name = "frm_DsaDataOwnerAdd";
+            this.Text = "Add a New Data Owner";
+            this.gb_DataOwners.ResumeLayout(false);
+            this.gb_DataOwners.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_DataOwners)).EndInit();
+            this.gb_AddDataOwner.ResumeLayout(false);
+            this.gb_AddDataOwner.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_NewDataOwnerName;
+        private System.Windows.Forms.Button btn_NewDataOwner;
+        private System.Windows.Forms.GroupBox gb_DataOwners;
+        private System.Windows.Forms.DataGridView dgv_DataOwners;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox tb_Search;
+        private System.Windows.Forms.GroupBox gb_AddDataOwner;
+        private System.Windows.Forms.ComboBox cb_RebrandingOfOldName;
+        private System.Windows.Forms.Label lbl_RebrandingOfOldName;
+        private System.Windows.Forms.Label lbl_NewDataOwnerName;
+    }
+}
---
+++ CMS/CMS/frm_DsaDataOwnerAdd.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataControlsLib.ViewModels;
+
+namespace CMS
+{
+    public partial class frm_DsaDataOwnerAdd : Form
+    {
+        public frm_DsaDataOwnerAdd()
+        {
+            InitializeComponent();
+            PopulateDsaDataset();
+            FillDataOwnerGridView();
+        }
+
+        private DataSet ds;
+
+        public void PopulateDsaDataset()
+        {
+            try
+            {
+                DSA dsa = new DSA();
+                ds = dsa.GetDsaData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to get DSA data from the database." + Environment.NewLine +
+                                ex.Message + Environment.NewLine +
+                                Environment.NewLine +
+                                ex.StackTrace);
+            }
+        }
+
+        public void FillDataOwnerGridView()
+        {
+            List<DataOwnersViewModel> doList = (
+                from do1 in ds.Tables["tblDsaDataOwners"].AsEnumerable()
+                join do2 in ds.Tables["tblDsaDataOwners"].AsEnumerable() on do1.Field<int?>("RebrandOf") equals do2.Field<int>("doID") into do2tmp
+                orderby do1.Field<string>("DataOwnerName")
+                from do2 in do2tmp.DefaultIfEmpty()
+                select new DataOwnersViewModel
+                {
+                    DataOwner = do1.Field<string>("DataOwnerName"),
+                    RebrandingOf = (do2 == null) ? null : do2.Field<string>("DataOwnerName")
+                })
+                .ToList();
+
+            dgv_DataOwners.DataSource = doList;
+            dgv_DataOwners.Columns["DataOwner"].Width = 150;
+            dgv_DataOwners.Columns["RebrandingOf"].Width = 150;
+        }
+    }
+}
