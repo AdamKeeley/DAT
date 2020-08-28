6184c3e06dbb1aaedf3fb3501d4eb254ede6263d
AdamKeeley <a.keeley@leeds.ac.uk>
2020-08-28 16:26:39 +0100

    Removed unnecessary usings

--- CMS/CMS/Program.cs
+++ CMS/CMS/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DataControlsLib;
 
@@ -16,7 +12,6 @@ namespace CMS
             /* TO DO
              * 
              * Search/All User
-             * Tidy Project forms
              * 
              * Documents 
              *     - incorporate some sort of 'timeline' of submitted documents?
