d82eaae338c32592d67c3b302b21082c0d1de563
AdamKeeley <a.keeley@leeds.ac.uk>
2020-12-11 11:29:34 +0000

    SQL update script corrected
    needs to drop constraint that is already present before it can add one.

--- sql/Update 2020-12-11.sql
+++ sql/Update 2020-12-11.sql
@@ -1,2 +1,5 @@
+-- First drop the constraint that defaults [LIDA] to 0
+alter table [dbo].[tblProject] drop constraint [DF__tblProject__LIDA__2A164134]
+-- then add new constraint that defaults [LIDA] to 1 
 ALTER TABLE [dbo].[tblProject] ADD  DEFAULT ((1)) FOR [LIDA]
 GO
